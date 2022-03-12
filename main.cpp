28170fd2198968d941e0b5c0f499534f6fdd0c18
sparrow9 <springsparrow9@163.com>
2022-03-12 10:10:56 +0800

    创建仓库

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include <qapplication.h>
+#include <qmainwindow.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QMainWindow main;
+    main.show();
+    return app.exec();
+}
+
+
